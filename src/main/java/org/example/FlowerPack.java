9bf994ea1df09eb4ce3d266da2fad7705f2309d4
sirandriy <andriyko.hlushko@gmail.com>
2023-10-12 20:36:35 +0300

    First commit

---
+++ src/main/java/org/example/FlowerPack.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class FlowerPack {
+    private Flower flower;
+    private int quantity;
+}
---
+++ src/main/java/org/example/FlowerType.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum FlowerType {
+    CHAMOMILE, ROSE, TULIP;
+}

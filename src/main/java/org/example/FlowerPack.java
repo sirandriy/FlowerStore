27d75f3105fd8a840926d89ba51735aa0d2496e7
sirandriy <andriyko.hlushko@gmail.com>
2023-10-12 21:49:15 +0300

    FlowerColor updated

--- src/main/java/org/example/FlowerPack.java
+++ src/main/java/org/example/FlowerPack.java
@@ -1,6 +1,21 @@
 package org.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
 public class FlowerPack {
     private Flower flower;
-    private int quantity;
+    private int flowers_quantity;
+
+    public FlowerPack(Flower flower, int amount) {
+        this.flower = flower;
+        this.flowers_quantity = amount;
+    }
+
+    public double getPrice() {
+        // Calculate the total price of the flower pack
+        return flower.getPrice() * flowers_quantity;
+    }
 }
+

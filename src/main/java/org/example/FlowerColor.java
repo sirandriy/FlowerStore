ec9667bf0ddfcf3338bf26dd7d561b4c9f7dbd63
sirandriy <andriyko.hlushko@gmail.com>
2023-10-12 21:58:10 +0300

    FlowerColor updated

--- src/main/java/org/example/FlowerColor.java
+++ src/main/java/org/example/FlowerColor.java
@@ -12,10 +12,20 @@ public enum FlowerColor {
 
     private final String stringRepresentation;
 
-    FlowerColor(String stringRepresentation) {
+    /**
+     * Constructor for FlowerColor.
+     *
+     * @param stringRepresentation The string representation of the color (e.g., "#FF0000" for RED).
+     */
+    FlowerColor(final String stringRepresentation) {
         this.stringRepresentation = stringRepresentation;
     }
 
+    /**
+     * Get the string representation of the color.
+     *
+     * @return The string representation of the color.
+     */
     @Override
     public String toString() {
         return stringRepresentation;

2f3e8a5d02760c028466546f91318e0cf84021af
sirandriy <andriyko.hlushko@gmail.com>
2023-10-12 21:53:01 +0300

    FlowerColor updated

--- src/main/java/org/example/FlowerColor.java
+++ src/main/java/org/example/FlowerColor.java
@@ -1,13 +1,21 @@
 package org.example;
 
+/**
+ * Enum representing flower colors.
+ */
 public enum FlowerColor {
-    RED("#FF0000"), YELLOW("#FFFF00"), BLUE("#0000FF"), GREEN("#00FF00"), PURPLE("#800080");
+    RED("#FF0000"),
+    YELLOW("#FFFF00"),
+    BLUE("#0000FF"),
+    GREEN("#00FF00"),
+    PURPLE("#800080");
 
     private final String stringRepresentation;
 
     FlowerColor(String stringRepresentation) {
         this.stringRepresentation = stringRepresentation;
     }
+
 
     @Override
     public String toString() {
